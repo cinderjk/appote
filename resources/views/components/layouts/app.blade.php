03e977ad77747141d43de5394e28bc2381ab74a3
cinderjk <cinderjk@gmail.com>
2023-11-29 10:36:44 +0700

    WIP: Add favicon, and fix sidebar, and add dark mode toggle

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,23 +1,34 @@
 <html>
 
 <head>
+    <script>
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="utf-8" />
     <title>{{ $title ?? 'Halaman' }} - {{ config('app.name') }}</title>
     <meta content="{{ config('app.name') }}" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
-    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-d595991b.css') }}"> --}}
+    {{--
+    <link rel="stylesheet" href="{{ asset('build/assets/app-d595991b.css') }}"> --}}
     @livewireStyles
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
-<body>
+<body class="dark:bg-gray-900">
 
-    <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
-        type="button"
-        class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500  sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+    <button wire:ignore data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar"
+        data-drawer-backdrop="false" type="button"
+        class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500  sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        id="openDrawer">
         <span class="sr-only">Open sidebar</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg">
@@ -27,12 +38,12 @@
         </svg>
     </button>
 
-    <aside id="default-sidebar"
+    <aside id="sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <a href="{{ route('dashboard') }}" class="flex items-center ps-2.5 mb-5">
-                <img src="{{ asset('assets/logo.png') }}" class="h-16" alt="Flowbite Logo" />
+                <img src="{{ asset('assets/logo.png') }}" class="h-16" alt="{{ config('app.name') }} Logo" />
             </a>
             <ul class="space-y-2 font-medium">
                 <li>
@@ -103,7 +114,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('logout') }}" wire:navigate
+                    <a href="{{ route('logout') }}"
                         class="flex items-center p-2 text-gray-900  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
@@ -114,6 +125,8 @@
                     </a>
                 </li>
             </ul>
+
+
         </div>
     </aside>
 
@@ -121,7 +134,74 @@
 
         {{ $slot }}
 
+        <button id="theme-toggle" type="button"
+            class="fixed bottom-5 right-5 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-3 bg-gray-200 dark:bg-gray-700">
+            <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+            </svg>
+            <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fill-rule="evenodd" clip-rule="evenodd"></path>
+            </svg>
+        </button>
     </div>
+
+
+    <script>
+        // get openDrawer button by id
+        var openDrawer = document.getElementById('openDrawer');
+        // on click, console log
+        openDrawer.addEventListener('click', function() {
+            // get sidebar by id
+            var sidebar = document.getElementById('sidebar');
+            // show sidebar
+            sidebar.classList.remove('-translate-x-full');
+        });
+
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
+        } else {
+            themeToggleDarkIcon.classList.remove('hidden');
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle');
+
+        themeToggleBtn.addEventListener('click', function() {
+
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                }
+
+            // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            }
+            
+        });
+    </script>
 
     {{-- <script src="{{ asset('build/assets/app-4a3c839c.js') }}"></script> --}}
     {{-- @livewireScripts --}}
--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,18 +1,26 @@
 <html>
 
 <head>
+    <script>
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="utf-8" />
     <title>{{ $title ?? 'Halaman' }} - {{ config('app.name') }}</title>
     <meta content="{{ config('app.name') }}" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
 
     @livewireStyles
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 
-<body class="flex items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+<body class="grid justify-items-stretch md:flex md:items-center md:justify-center h-screen bg-gray-100 dark:bg-gray-900">
     {{ $slot }}
 
     @livewireScriptConfig
